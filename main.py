13da30718a63d0863422928ddede381f7e9d42e9
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2023-05-06 21:48:56 +0200

    wip

--- main.py
+++ main.py
@@ -1,75 +1,129 @@
 import tkinter as tk
 from tkinter import ttk
-import sqlite3
+from tkinter import filedialog
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+
+# Global variable to store the image path
+image_path = None
 
 
-def create_table():
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS people (name TEXT, age INTEGER)")
-    conn.commit()
-    conn.close()
+def add_entry():
+    values = [entries[field].get() for field in entry_labels]
+    if all(values) and image_path:
+        treeview.insert("", tk.END, values=values)
+    else:
+        messagebox.showerror("Error", "Please fill in all fields and select an image.")
 
 
-def add_item():
-    name = name_var.get()
-    age = age_var.get()
-    if name and age:
-        tree.insert("", "end", values=(name, age))
-        insert_data(name, age)
-        name_var.set("")
-        age_var.set("")
+def remove_entry():
+    selected_item = treeview.selection()
+    if selected_item:
+        treeview.delete(selected_item)
+    else:
+        messagebox.showerror("Error", "Please select an item to remove.")
 
 
-def insert_data(name, age):
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
-    c.execute("INSERT INTO people (name, age) VALUES (?, ?)", (name, age))
-    conn.commit()
-    conn.close()
+def show_record():
+    selected_item = treeview.selection()
+    if selected_item:
+        record = treeview.item(selected_item)["values"]
+        messagebox.showinfo(
+            "Record",
+            "\n".join(
+                [f"{entry_labels[i]}: {value}" for i, value in enumerate(record)]
+            ),
+        )
+    else:
+        messagebox.showerror("Error", "Please select an item to show the record.")
 
 
-def load_data():
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM people")
-    records = c.fetchall()
-    conn.close()
-    return records
+def print_report():
+    pass
 
 
-create_table()
+def print_barcode():
+    pass
 
+
+def print_barcode_sticker():
+    pass
+
+
+def go_to_admin_page():
+    pass
+
+
+def browse_file():
+    global image_path
+    image_path = filedialog.askopenfilename(
+        filetypes=[("Image files", "*.jpg;*.png;*.jpeg")]
+    )
+    # You can display the selected image using a library like Pillow or OpenCV.
+    pass
+
+
+# The rest of the code remains the same, just replace the function definitions with the new ones.
+
+
+# Create the main window
 root = tk.Tk()
-root.title("Treeview and Data Entry GUI with SQLite")
+root.title("TreeView and Data Entry GUI")
 
-frame = ttk.Frame(root, padding="3")
-frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+# Create the treeview
+treeview = ttk.Treeview(
+    root,
+    columns=("titel", "medium", "afmeting", "categorie", "jaar", "prijs"),
+    show="headings",
+)
+treeview.heading("titel", text="Titel")
+treeview.heading("medium", text="Medium")
+treeview.heading("afmeting", text="Afmeting")
+treeview.heading("categorie", text="Categorie")
+treeview.heading("jaar", text="Jaar")
+treeview.heading("prijs", text="Prijs")
+treeview.pack()
 
-tree = ttk.Treeview(frame, columns=("Name", "Age"), show="headings")
-tree.heading("Name", text="Name")
-tree.heading("Age", text="Age")
-tree.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+# Create data entry labels and entry fields
+entry_labels = ["Titel", "Medium", "Afmeting", "Categorie", "Jaar", "Prijs"]
+entries = {}
+for i, label in enumerate(entry_labels):
+    tk.Label(root, text=label).grid(row=i, column=0)
+    entries[label.lower()] = tk.Entry(root)
+    entries[label.lower()].grid(row=i, column=1)
 
-for record in load_data():
-    tree.insert("", "end", values=(record[0], record[1]))
+# Add a label and button to select a photo
+photo_label = tk.Label(root, text="Foto")
+photo_label.grid(row=len(entry_labels), column=0)
 
-form_frame = ttk.Frame(frame, padding="3")
-form_frame.grid(row=0, column=1, padx=5, sticky=(tk.W, tk.E, tk.N, tk.S))
+browse_button = tk.Button(root, text="Bladeren", command=browse_file)
+browse_button.grid(row=len(entry_labels), column=1)
 
-name_label = ttk.Label(form_frame, text="Name:")
-name_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-name_var = tk.StringVar()
-name_entry = ttk.Entry(form_frame, textvariable=name_var)
-name_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+# Create buttons
+button_frame = tk.Frame(root)
+button_frame.pack()
 
-age_label = ttk.Label(form_frame, text="Age:")
-age_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-age_var = tk.StringVar()
-age_entry = ttk.Entry(form_frame, textvariable=age_var)
-age_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+toevoegen_button = tk.Button(button_frame, text="Toevoegen", command=add_entry)
+toevoegen_button.pack(side=tk.LEFT)
 
-add_button = ttk.Button(form_frame, text="Add", command=add_item)
-add_button.grid(row=2, columnspan=2, pady=10)
+verwijderen_button = tk.Button(button_frame, text="Verwijderen", command=remove_entry)
+verwijderen_button.pack(side=tk.LEFT)
+
+toon_record_button = tk.Button(button_frame, text="Toon record", command=show_record)
+toon_record_button.pack(side=tk.LEFT)
+
+# Create bottom menu
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="Functionaliteiten", menu=file_menu)
+file_menu.add_command(label="Print Rapport", command=print_report)
+file_menu.add_command(label="Print Barcode", command=print_barcode)
+file_menu.add_command(label="Print Barcode Sticker", command=print_barcode_sticker)
+file_menu.add_command(label="Ga naar Admin Pagina", command=go_to_admin_page)
 
 root.mainloop()
