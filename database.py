3e3696a28693569099bdf3c2c0aa1a39e4657c53
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2023-05-06 22:50:25 +0200

    go

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
 import sqlite3
 
+
 class Database:
     def __init__(self):
         self.conn = sqlite3.connect("database.db")
         self.cursor = self.conn.cursor()
         self.create_table()
-    
+
     def create_table(self):
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS items (
                 id INTEGER PRIMARY KEY,
                 title TEXT,
@@ -17,25 +19,32 @@ class Database:
                 year INTEGER,
                 price REAL
             )
-        """)
+        """
+        )
         self.conn.commit()
-    
+
     def insert(self, data):
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
             INSERT INTO items (title, medium, dimension, category, year, price) VALUES (?, ?, ?, ?, ?, ?)
-        """, data)
+        """,
+            data,
+        )
         self.conn.commit()
-    
+
     def update(self, data):
-        self.cursor.execute("""
+        self.cursor.execute(
+            """
             UPDATE items SET title=?, medium=?, dimension=?, category=?, year=?, price=? WHERE id=?
-        """, data)
+        """,
+            data,
+        )
         self.conn.commit()
-    
+
     def delete(self, item_id):
         self.cursor.execute("DELETE FROM items WHERE id=?", (item_id,))
         self.conn.commit()
-    
+
     def get_all(self):
         self.cursor.execute("SELECT * FROM items")
         return self.cursor.fetchall()
--- gui_elements.py
+++ gui_elements.py
@@ -1,16 +1,118 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+
 
 class GUIElements:
     def __init__(self, parent, db):
         self.parent = parent
         self.db = db
-        
+        self.selected_item = None
+
         self.create_gui_elements()
-    
+
     def create_gui_elements(self):
-        # Maak hier de GUI-elementen zoals beschreven in de ASCII-layout
-        # (labels, tekstvakken, knoppen, etc.)
-        pass  # Verwijder deze regel en voeg uw GUI-code toe
-        
-    # Event handlers voor knoppen, tekstvakken, enz. toevoegen
+        # Frame voor de inputvelden en labels
+        input_frame = tk.Frame(self.parent)
+        input_frame.grid(row=0, column=0, sticky="nsew")
+
+        # Maak labels en inputvelden
+        labels = ["Titel", "Medium", "Afmeting", "Categorie", "Jaar", "Prijs"]
+        self.entries = {}
+        for index, label in enumerate(labels):
+            tk.Label(input_frame, text=label).grid(row=index, column=0)
+            entry = tk.Entry(input_frame, width=20)
+            entry.grid(row=index, column=1)
+            self.entries[label.lower()] = entry
+
+        # Database tabel
+        self.tree = ttk.Treeview(self.parent, columns=labels, show="headings")
+        for label in labels:
+            self.tree.heading(label, text=label)
+        self.tree.grid(row=1, column=0, columnspan=2)
+
+        # Bind event handler om geselecteerd item bij te werken
+        self.tree.bind("<<TreeviewSelect>>", self.on_tree_select)
+
+        # Laad gegevens in de tabel
+        self.load_data()
+
+        # Knoppen
+        button_frame = tk.Frame(self.parent)
+        button_frame.grid(row=2, column=0, columnspan=2, pady=(10, 0))
+
+        # Knoppen en bijbehorende functies
+        buttons = [
+            ("Toevoegen", self.on_add),
+            ("Bijwerken", self.on_update),
+            ("Verwijderen", self.on_delete),
+            ("Filteren", self.on_filter),
+        ]
+        for index, (text, command) in enumerate(buttons):
+            tk.Button(button_frame, text=text, command=command).grid(
+                row=0, column=index, padx=(0, 10)
+            )
+
+    def load_data(self):
+        # Verwijder alle items uit de tabel
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+
+        # Voeg items uit de database toe aan de tabel
+        for item in self.db.get_all():
+            self.tree.insert("", "end", values=item)
+
+    def on_tree_select(self, event):
+        item = self.tree.selection()[0]
+        self.selected_item = self.tree.item(item)["values"]
+
+        # Vul de invoervelden met de gegevens van het geselecteerde item
+        for index, key in enumerate(self.entries):
+            self.entries[key].delete(0, tk.END)
+            self.entries[key].insert(0, str(self.selected_item[index]))
+
+    def on_add(self):
+        # Haal de gegevens op uit de inputvelden en voeg ze toe aan de database
+        data = tuple(entry.get() for entry in self.entries.values())
+        self.db.insert(data)
+        self.load_data()
+
+    def on_update(self):
+        # Werk het geselecteerde item bij met de gegevens uit de inputvelden
+        if self.selected_item:
+            updated_data = tuple(entry.get() for entry in self.entries.values()) + (
+                self.selected_item[0],
+            )
+            self.db.update(updated_data)
+            self.load_data()
+
+    def on_delete(self):
+        # Verwijder het geselecteerde item uit de database en de tabel
+        if self.selected_item:
+            confirm = messagebox.askyesno(
+                "Bevestig verwijderen", "Weet u zeker dat u dit item wilt verwijderen?"
+            )
+            if confirm:
+                self.db.delete(self.selected_item[0])
+                self.load_data()
+                self.selected_item = None
+                for entry in self.entries.values():
+                    entry.delete(0, tk.END)
+
+    def on_filter(self):
+        # Filter de tabel op basis van de gegevens in de inputvelden
+        filter_values = tuple(entry.get() for entry in self.entries.values())
+
+        # Verwijder alle items uit de tabel
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+
+        # Voeg gefilterde items uit de database toe aan de tabel
+        for item in self.db.get_all():
+            match = True
+            for index, value in enumerate(filter_values):
+                if value and str(item[index]) != value:
+                    match = False
+                    break
+            if match:
+                self.tree.insert("", "end", values=item)
