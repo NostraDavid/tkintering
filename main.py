c783661ef7d9d28a1137346b669069cbdfbd42fa
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2023-05-06 21:36:14 +0200

    closer

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+import sqlite3
+
+
+def create_table():
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS people (name TEXT, age INTEGER)")
+    conn.commit()
+    conn.close()
 
 
 def add_item():
@@ -7,12 +16,32 @@ def add_item():
     age = age_var.get()
     if name and age:
         tree.insert("", "end", values=(name, age))
+        insert_data(name, age)
         name_var.set("")
         age_var.set("")
 
 
+def insert_data(name, age):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO people (name, age) VALUES (?, ?)", (name, age))
+    conn.commit()
+    conn.close()
+
+
+def load_data():
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM people")
+    records = c.fetchall()
+    conn.close()
+    return records
+
+
+create_table()
+
 root = tk.Tk()
-root.title("Treeview and Data Entry GUI")
+root.title("Treeview and Data Entry GUI with SQLite")
 
 frame = ttk.Frame(root, padding="3")
 frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
@@ -21,6 +50,9 @@ tree = ttk.Treeview(frame, columns=("Name", "Age"), show="headings")
 tree.heading("Name", text="Name")
 tree.heading("Age", text="Age")
 tree.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+for record in load_data():
+    tree.insert("", "end", values=(record[0], record[1]))
 
 form_frame = ttk.Frame(frame, padding="3")
 form_frame.grid(row=0, column=1, padx=5, sticky=(tk.W, tk.E, tk.N, tk.S))
